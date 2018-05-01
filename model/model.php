ab6affdab69e6db58cbb2971849eece78783378f
Ryan Sung <ryansung@WL-204-20.WPA.Claremont.Edu>
2018-05-01 12:20:58 -0700

    changes

--- model/model.php
+++ model/model.php
@@ -6,7 +6,7 @@ class Model {
  	{
  		// here goes some hardcoded values to simulate the database
   		if(isset($_REQUEST['username']) && isset($_REQUEST['password'])){
-  
+
    			if($_REQUEST['username']=='admin' && $_REQUEST['password']=='admin'){
     			return 'login';
    			}
@@ -15,7 +15,7 @@ class Model {
    			}
   		}
  	}
- 
+
 }
 
 ?>
