1728ce33edee1c59771fe8288f446dffbe4a3522
Ryan Sung <ryansung@WL-203-21.WPA.Claremont.Edu>
2018-05-02 13:22:02 -0700

    adding view.php

---
+++ view/view.php
@@ -0,0 +1,29 @@
+<?php
+
+class View {
+	public function __construct($model)  {
+      $this->model = $model;
+  	}
+
+	// public function renderCart($shoppingCart) {
+  //     	include_once("viewcart.php");
+ 	// }
+  //
+	// public function renderOrderForm($cookieTypes){
+  //   	include_once("orderForm.php");
+	// }
+
+	public function renderLoginPage {
+
+	}
+
+	public function renderHomePage {
+
+	}
+
+	public function registerPage {
+
+	}
+}
+
+?>
